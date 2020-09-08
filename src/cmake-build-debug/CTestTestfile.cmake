7cae09503a5a095d346beeaa5af59cc9a57461a8
Daniel Rowan <none@none.com>
2020-09-07 19:30:43 -0700

    first commit of scte rc car based ROS robot

---
+++ src/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/ros_ws/scteBot_ws/src
+# Build directory: /home/daniel/ros_ws/scteBot_ws/src/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("urdf_models/scte_vehicle/scte_description")
+subdirs("urdf_models/scte_vehicle/scte_gazebo")
---
+++ src/cmake-build-debug/urdf_models/scte_vehicle/scte_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/ros_ws/scteBot_ws/src/urdf_models/scte_vehicle/scte_gazebo
+# Build directory: /home/daniel/ros_ws/scteBot_ws/src/cmake-build-debug/urdf_models/scte_vehicle/scte_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
