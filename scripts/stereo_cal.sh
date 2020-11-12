a2977000e4d1fc151ae921274f15e27313c81c83
Daniel <omnisurfer@gmail.com>
2020-11-11 18:18:32 -0800

    more tweaks to stereo camera

--- scripts/stereo_cal.sh
+++ scripts/stereo_cal.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
 rosrun camera_calibration cameracalibrator.py \
-approximate 0.1 --size 8x6 --square 0.026 \
+approximate 0.1 --size 8x6 --square 0.108 \
 right:=/stereo/c270_right/image_raw right_camera:=/stereo/c270_right \
 left:=/stereo/c270_left/image_raw left_camera:=/stereo/c270_left
