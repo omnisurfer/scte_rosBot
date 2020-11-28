38dbacb83d78689575141efe3826a4e3726fe371
Daniel <omnisurfer@gmail.com>
2020-11-27 17:27:35 -0800

    update script to change tty setting before going to 10Hz

--- env/config_pi_gps.sh
+++ env/config_pi_gps.sh
@@ -6,11 +6,11 @@ systemctl stop gpsd.socket & wait
 echo "Set baud..."
 echo -e "\$PMTK251,115200*1F\r\n" > /dev/ttyS0
 sleep 3
-echo "Set 10Hz..."
-echo -e "\$PMTK220,100*2F\r\n" > /dev/ttyS0
-sleep 3
 echo "Config ttyS0..."
 stty -F /dev/ttyS0 speed 115200
+sleep 3
+echo "Set 10Hz..."
+echo -e "\$PMTK220,100*2F\r\n" > /dev/ttyS0
 sleep 3
 
 systemctl start gpsd & wait
