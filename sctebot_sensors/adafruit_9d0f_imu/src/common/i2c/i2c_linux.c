759fc33144d2cbb94a04c22d62db4004d2d3cfe5
Daniel Rowan <omnisurfer@gmail.com>
2021-04-09 11:45:49 -0700

    started basic code for adafruit 9dof i2c

---
+++ sctebot_sensors/adafruit_9d0f_imu/src/common/i2c/i2c_linux.c
@@ -0,0 +1,136 @@
+//
+// Created by user on 4/9/21.
+//
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <string.h>
+
+#include "i2c_linux.h"
+
+/*
+ * @brief   Performs either writing or reading operation from I2C device
+ *
+ * @param   [in]        context device context
+ * @param   [in/out]    message: message contains buffer with information on
+ *                      how to store or retreive information.
+ *                      If message->write is true, data will be stored, otherwise read
+ *
+ * @returns             1 on success, otherwise 0
+ */
+
+static int i2c_operation(const context_t* context, message_t *message) {
+    // check if connected
+    if (!context->device) {
+        return 0;
+    }
+
+    struct i2c_smbus_ioctl_data smbus_args;
+    union i2c_smbus_data smbus_data;
+    int result = 1;
+
+    smbus_args.read_write = message->operation_is_write ? I2C_SMBUS_WRITE : I2C_SMBUS_READ;
+    smbus_args.command = message->i2c_register;
+    smbus_args.size = I2C_SMBUS_BYTE_DATA;
+    smbus_args.data = &smbus_data;
+
+    size_t data_length = message->buffer->size;
+
+    for(size_t i=0; i < data_length; i++) {
+        smbus_data.byte = message->buffer->bytes[i];
+        smbus_args.command = message->i2c_register + i;
+        if(ioctl(context->device, I2C_SMBUS, &smbus_args) < 0) {
+            return 0;
+        }
+        if (!message->operation_is_write) {
+            message->buffer->bytes[i] = smbus_data.byte;
+            message->buffer->size = i + 1;
+        }
+    }
+
+    return result;
+}
+
+int i2c_send(const context_t *context, const buffer_t *plaintext) {
+    uint8_t encoded_data[MAX_BUFFER_SIZE] = {0};
+
+    message_t message = {
+            .buffer = plaintext,
+            .i2c_register = 0,
+            .operation_is_write = 1,
+    };
+
+    return i2c_operation(context, &message);
+}
+
+int i2c_dev_open(context_t *context, int device_number, int slave_address) {
+    char filename[64];
+    sprintf(filename, "/dev/i2c-%d", device_number);
+
+    if((context->device = open(filename, O_RDWR)) < 0) {
+        printf("Failed to open /dev/i2c-%d: %s\n", device_number, strerror(errno));
+        return 0;
+    }
+
+    if(ioctl(context->device, I2C_SLAVE, slave_address) < 0) {
+        printf("Failed to acquire bus access at %d", slave_address);
+        return 0;
+    }
+
+    printf("/dev/i2c-%d opened!\r\n", device_number);
+
+    return 1;
+}
+
+int i2c_is_connected(context_t *context) {
+    // Device failed to open
+    if(!context || !context->device) {
+        return 0;
+    }
+
+    // Check if slave respondes to reads
+    uint8_t temp[1];
+    buffer_t buffer = {
+            .bytes = temp,
+            .size = 1
+    };
+
+    message_t message = {
+            .buffer = &buffer,
+            .operation_is_write = 0,
+            .i2c_register = 0
+    };
+
+    return i2c_operation(context, &message);
+}
+
+int main(int argc, char* argv[]) {
+    printf("i2c_linux\n");
+
+    context_t context = {0};
+    int device_id = 0;
+
+    uint8_t outputBuffer[MAX_BUFFER_SIZE] = {0};
+
+    uint8_t inputBuffer[] = "Hello World";
+
+    int device_open = i2c_dev_open(&context, device_id, 0x03);
+    int is_connected = i2c_is_connected(&context);
+
+    buffer_t temp = {
+            .bytes = inputBuffer,
+            .size = sizeof(inputBuffer)
+    };
+
+    int message_sent = i2c_send(&context, &temp);
+
+    return 0;
+}
---
+++ sctebot_sensors/adafruit_9d0f_imu/src/common/i2c/i2c_linux.h
@@ -0,0 +1,78 @@
+//
+// Created by user on 4/9/21.
+//
+
+// I2C based on code from https://git.amongbytes.com/kris/i2c-stub-toy/src/branch/master/src/i2c.c
+
+#ifndef I2C_LINUX_H_
+#define I2C_LINUX_H_
+
+// max message size in bytes. represents number of registers accessible in i2c-stub
+#define MAX_BUFFER_SIZE 256U
+
+// buffer struct
+typedef struct buffer_t {
+    uint8_t *bytes;
+    size_t size;
+}buffer_t;
+
+// Device connection context
+typedef struct context_t {
+    int device;
+} context_t;
+
+// Stores a buffer message to write or that was read
+typedef struct message_t {
+    buffer_t *buffer;          // data to be sent, or where received data is stored
+    uint8_t i2c_register;       // register to read or write to?
+    int operation_is_write; // indicate operation is write = 1, read = 0
+} message_t;
+
+/*
+ * @brief   Opens a persistent connection to I2C slave device
+ *
+ * @param   [out]   context device context
+ * @param   [in]    device_num character device which should be used for connection
+ *                  For example, if device is /dev/i2c-8, then device_num=8
+ * @param   [in]    slave_address of the I2C slave located on the device
+ * @returns         1 on success and sets 'context', 0 otherwise
+ */
+int i2c_dev_open(context_t *context, int device_num, int slave_id);
+
+/*
+ * @brief   Closes connection to I2C slave
+ *
+ * @param   [in]    slave_addr address of the I2C slave.
+ *
+ * @returns         none
+ */
+void i2c_dev_close(context_t *context);
+
+/*
+ * @brief   Checks if connection is opened and I2C slave is responding to commands
+ *
+ * @param   [in]    context device context
+ * @returns         1 on success, 0 otherwise
+ */
+int i2c_is_connected(context_t *context);
+
+/*
+ * @brief   Sends data to I2C device
+ *
+ * @param   [in]    context device context
+ * @param   [in]    plaintext - buffer with a plaintext to send
+ * @returns         1 on success, 0 otherwise
+ */
+int i2c_send(const context_t *context, const buffer_t *plaintext);
+
+/*
+ * @brief   Receives data from I2C slave
+ *
+ * @param   [in]    context device context
+ * @param   [out]   plaintext - buffer to store received data in. Buffer
+ *                  must be big enough to store data + TAG_LEN bytes.
+ * @returns         1 on success, 0 otherwise
+ */
+int i2c_recv(const context_t *context, buffer_t *plaintext);
+
+#endif //I2C_LINUX_H_
