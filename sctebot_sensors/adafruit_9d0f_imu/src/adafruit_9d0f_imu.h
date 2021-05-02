4570d1b24098babe0c13cafdecf98d39492f1440
Daniel Rowan <omnisurfer@gmail.com>
2021-05-02 14:03:03 -0700

    start moving adafruit code into actual class

--- sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9d0f_imu.h
+++ sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9d0f_imu.h
@@ -5,4 +5,179 @@
 #ifndef ADAFRUIT_9D0F_IMU_ADAFRUIT_9D0F_IMU_H
 #define ADAFRUIT_9D0F_IMU_ADAFRUIT_9D0F_IMU_H
 
+#include <iostream>
+#include <thread>
+#include <utility>
+
+#include "common/i2c/i2c_linux.h"
+#include "common/devices/bmp180.h"
+
+void demo_thread() {
+    std::cout << "demo thread printing text" << std::endl;
+}
+
+class AdaFruit9DoFImu {
+
+private:
+    int _i2c_bus_number;
+    int _i2c_device_address;
+    std::string _device_name;
+
+    context_t _ada_i2c_context;
+
+    uint8_t _calibration_data_buffer[11 * 2] = {0};
+
+public:
+    AdaFruit9DoFImu(int bus_number, int device_address, std::string device_name) {
+        _i2c_bus_number = bus_number;
+        _i2c_device_address = device_address;
+        _device_name = std::move(device_name);
+    }
+
+
+    ~AdaFruit9DoFImu() {
+        std::cout << "destructor called" << std::endl;
+    }
+
+    int connect_to_device() {
+
+        /*
+         * setup the i2c context and connect
+         */
+        _ada_i2c_context = {0};
+        if(!i2c_dev_open(&_ada_i2c_context, _i2c_bus_number, _i2c_device_address)) {
+            std::cout << "failed to open device\n";
+            return -1;
+        }
+
+        if(!i2c_is_connected(&_ada_i2c_context)) {
+            std::cout << "failed to connect to device\n";
+            return -1;
+        }
+
+        return 0;
+    }
+
+    int load_mock_calibration_data() {
+
+        /*
+         * RPI4 is little endian, x86 = little endian  (use lscpu | grep -i byte)
+         * https://betterexplained.com/articles/understanding-big-and-little-endian-byte-order/
+         */
+
+        /* Memory dump from real device @0x77 on ros-pi-node
+        00: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        10: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        20: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        30: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        40: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        50: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        60: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        70: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        80: a5 94 45 09 8d 27 29 3e 7a 55 1e d4 fb b2 c7 27    ??E??')>zU?????'
+        90: 83 3b 63 e7 42 65 19 73 00 28 80 00 d1 f6 09 a6    ?;c?Be?s.(?.????
+        a0: a5 94 45 09 8d 27 29 3e 7a 55 1e d4 fb b2 c7 27    ??E??')>zU?????'
+        b0: 83 3b 63 e7 42 65 19 73 00 28 80 00 d1 f6 09 a6    ?;c?Be?s.(?.????
+        c0: 00 00 bc 33 00 00 00 00 00 00 00 10 00 00 00 03    ..?3.......?...?
+        d0: 55 02 06 00 00 00 00 00 00 00 00 00 00 00 00 00    U??.............
+        e0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
+        f0: 00 00 00 00 00 00 80 00 00 00 00 00 00 00 00 00    ......?.........
+         */
+
+        /* Calibration data from real sensor
+        d41e b2fb 27c7
+        3b83 e763 6542 7319 2800 0080 f6d1 a609
+         */
+
+
+        /*
+         * real temp 0x6496
+         * real pressure 0xa307 0x00
+         */
+
+        static uint16_t _mock_device_memory[] = {
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0xa594, 0x4509, 0x8d27, 0x293e, 0x7a55, 0x1ed4, 0xfbb2, 0xc727,
+                0x833b, 0x63e7, 0x4265, 0x1973, 0x0028, 0x8000, 0xd1f6, 0x09a6,
+                0xa594, 0x4509, 0x8d27, 0x293e, 0x7a55, 0x1ed4, 0xfbb2, 0xc727,
+                0x833b, 0x63e7, 0x4265, 0x1973, 0x0028, 0x8000, 0xd1f6, 0x09a6,
+                0x0000, 0xbc33, 0x0000, 0x0000, 0x0000, 0x0010, 0x0000, 0x0003,
+                0x5502, 0x0600, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
+                0x0000, 0x0000, 0x0000, 0x6496, 0x0000, 0x0000, 0x0000, 0x0000
+        };
+
+
+        uint8_t send_buffer[sizeof(_mock_device_memory)];
+        std::memset(&send_buffer, 0, sizeof(send_buffer));
+
+        std::memcpy(&send_buffer, _mock_device_memory, sizeof(_mock_device_memory));
+
+        buffer_t outbound_message = {
+                .bytes = send_buffer,
+                .size = sizeof(send_buffer)
+        };
+
+        int8_t register_address = 0x00;
+
+        if (i2c_send(&_ada_i2c_context, &outbound_message, register_address)) {
+            std::cout << "sent mock calibration data to device OK\n";
+            return 0;
+        }
+
+        return -1;
+    }
+
+    int init_device() {
+
+        buffer_t inbound_message = {
+                .bytes = _calibration_data_buffer,
+                .size = sizeof(_calibration_data_buffer)
+        };
+
+        uint8_t register_address = (Bmp180::Addresses::CalibrationCoefficients::AC1 >> 8) & 0xff;
+        if (i2c_recv(&_ada_i2c_context, &inbound_message, register_address)) {
+
+            std::cout << "calibration data: " << std::endl;
+
+            for(int i = 0; i < sizeof(_calibration_data_buffer); ++i) {
+                std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)_calibration_data_buffer[i] << " ";
+            }
+
+            std::cout << std::endl;
+        }
+
+        return 0;
+    }
+
+    void start_demo_thread() {
+
+        std::thread demo_thread_object(demo_thread);
+
+        std::cout << "start_demo_thread call" << std::endl;
+
+        demo_thread_object.join();
+
+    }
+
+    void read_temperature();
+
+    void read_pressure();
+
+    uint8_t* get_calibration_buffer_address() {
+        return _calibration_data_buffer;
+    }
+
+    int get_calibration_buffer_size() {
+        return sizeof(_calibration_data_buffer);
+    }
+};
+
 #endif //ADAFRUIT_9D0F_IMU_ADAFRUIT_9D0F_IMU_H
--- sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9dof_imu.cpp
+++ sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9dof_imu.cpp
@@ -12,124 +12,56 @@
 #include "common/i2c/i2c_linux.h"
 #include "common/devices/bmp180.h"
 
-class AdaFruit9DoFImu {
-public:
-    int my_num;
-    std::string name_of_device;
-};
+#include "adafruit_9d0f_imu.h"
 
 
-void Bmp180WriteMockCoefficientsToMemory(char* mock_coefficients, int16_t array_size) {
+void AdaFruit9DoFImu::read_temperature() {
 
-    context_t ada_i2c_context = {0};
-    int device_id = 0;
-    int slave_id = 0x03;
-
-    if(!i2c_dev_open(&ada_i2c_context, device_id, slave_id)) {
-        std::cout << "failed to open device\n";
-    }
-
-    if(!i2c_is_connected(&ada_i2c_context)) {
-        std::cout << "failed to connect to device\n";
-    }
-
-    uint8_t send_buffer[array_size];
-    std::memset(&send_buffer, 0, sizeof(send_buffer));
-
-    std::memcpy(&send_buffer, mock_coefficients, array_size);
-
-    buffer_t outbound_message = {
-            .bytes = send_buffer,
-            .size = sizeof(send_buffer)
-    };
-
-    int8_t register_address = 0x00;
-
-    if (i2c_send(&ada_i2c_context, &outbound_message, register_address)) {
-        std::cout << "sent buffer to device OK\n";
-    }
-
-    i2c_dev_close(&ada_i2c_context, device_id);
 }
 
+void AdaFruit9DoFImu::read_pressure() {
+
+}
 
 int main(int argc, char* argv[]) {
     std::cout << "Hello World adafruit 9dof IMU" << std::endl;
 
-    int i2c_device_number = 0;
-    int device_address = 0x03;
+    int i2c_bus_number = 0;
+    int i2c_device_address = 0x03;
 
     // lame way to do this but good enough for debug
     if(argv[1]) {
         if (!memcmp("-d", argv[1], 2)) {
-            i2c_device_number = atoi(argv[2]);
+            i2c_bus_number = atoi(argv[2]);
         }
     }
 
     if(argv[3]) {
         if(!memcmp("-a", argv[3], 2)) {
-            device_address = std::stoi(argv[4], 0, 16);
+            i2c_device_address = std::stoi(argv[4], 0, 16);
         }
     }
 
-    /*
-     * RPI4 is little endian, x86 = little endian  (use lscpu | grep -i byte)
-     * https://betterexplained.com/articles/understanding-big-and-little-endian-byte-order/
-     */
+    AdaFruit9DoFImu adaFruit9DoFImu = AdaFruit9DoFImu(i2c_bus_number, i2c_device_address, "pressure sensor");
 
-    /* Memory dump from real device @0x77 on ros-pi-node
-    00: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    10: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    20: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    30: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    40: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    50: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    60: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    70: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    80: a5 94 45 09 8d 27 29 3e 7a 55 1e d4 fb b2 c7 27    ??E??')>zU?????'
-    90: 83 3b 63 e7 42 65 19 73 00 28 80 00 d1 f6 09 a6    ?;c?Be?s.(?.????
-    a0: a5 94 45 09 8d 27 29 3e 7a 55 1e d4 fb b2 c7 27    ??E??')>zU?????'
-    b0: 83 3b 63 e7 42 65 19 73 00 28 80 00 d1 f6 09 a6    ?;c?Be?s.(?.????
-    c0: 00 00 bc 33 00 00 00 00 00 00 00 10 00 00 00 03    ..?3.......?...?
-    d0: 55 02 06 00 00 00 00 00 00 00 00 00 00 00 00 00    U??.............
-    e0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00    ................
-    f0: 00 00 00 00 00 00 80 00 00 00 00 00 00 00 00 00    ......?.........
-     */
-
-    uint16_t actual_device_memory[] = {
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0xa594, 0x4509, 0x8d27, 0x293e, 0x7a55, 0x1ed4, 0xfbb2, 0xc727,
-     0x833b, 0x63e7, 0x4265, 0x1973, 0x0028, 0x8000, 0xd1f6, 0x09a6,
-     0xa594, 0x4509, 0x8d27, 0x293e, 0x7a55, 0x1ed4, 0xfbb2, 0xc727,
-     0x833b, 0x63e7, 0x4265, 0x1973, 0x0028, 0x8000, 0xd1f6, 0x09a6,
-     0x0000, 0xbc33, 0x0000, 0x0000, 0x0000, 0x0010, 0x0000, 0x0003,
-     0x5502, 0x0600, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-     0x0000, 0x0000, 0x0000, 0x6496, 0x0000, 0x0000, 0x0000, 0x0000
-    };
-
-    /*
-     * real temp 0x6496
-     * real pressure 0xa307 0x00
-     */
+    adaFruit9DoFImu.start_demo_thread();
 
     uint16_t mock_bmp180_uncompensated_temp = 0x6496;
     uint16_t mock_bmp180_uncompensated_pressure = 0xa307;
     uint8_t mock_bmp180_uncompensated_pressure_xlsb = 0x00;
 
+    adaFruit9DoFImu.connect_to_device();
+
+    adaFruit9DoFImu.load_mock_calibration_data();
+
+    adaFruit9DoFImu.init_device();
+
     /*
      * setup the i2c context and connect
      */
     context_t ada_i2c_context = {0};
-    int device_id = i2c_device_number;
-    int slave_id = device_address;
+    int device_id = i2c_bus_number;
+    int slave_id = i2c_device_address;
 
     if(!i2c_dev_open(&ada_i2c_context, device_id, slave_id)) {
         std::cout << "failed to open device\n";
@@ -140,36 +72,6 @@ int main(int argc, char* argv[]) {
         std::cout << "failed to connect to device\n";
         return -1;
     }
-
-    /* Write out mock data into the mock device */
-    Bmp180WriteMockCoefficientsToMemory((char *) actual_device_memory, sizeof(actual_device_memory));
-
-    /*
-     * prepare to read the params and data
-     */
-    uint8_t calibration_data_buffer[11 * 2] = {0};
-
-    buffer_t inbound_message = {
-            .bytes = calibration_data_buffer,
-            .size = sizeof(calibration_data_buffer)
-    };
-
-    uint8_t register_address = (Bmp180::Addresses::CalibrationCoefficients::AC1 >> 8) & 0xff;
-    if (i2c_recv(&ada_i2c_context, &inbound_message, register_address)) {
-
-        std::cout << "calibration data: " << std::endl;
-
-        for(int i = 0; i < sizeof(calibration_data_buffer); ++i) {
-            std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)calibration_data_buffer[i] << " ";
-        }
-
-        std::cout << std::endl;
-    }
-
-    /* Calibration data from real sensor
-    d41e b2fb 27c7
-    3b83 e763 6542 7319 2800 0080 f6d1 a609
-     */
 
     // command temperature read
     // notify bmp180 to read temp reg
@@ -180,6 +82,9 @@ int main(int argc, char* argv[]) {
             .bytes = control_register_and_start_meas,
             .size = 1
     };
+
+    uint8_t register_address;
+    buffer_t inbound_message;
 
     register_address = Bmp180::Addresses::DataRegisters::CONTROL_MEASUREMENT;
     if (i2c_send(&ada_i2c_context, &outbound_message, register_address)) {
@@ -253,7 +158,9 @@ int main(int argc, char* argv[]) {
      * Start calculating measurements with the Bmp180 code
      */
     Bmp180 bmp180 = Bmp180();
-    bmp180.init_bmp180_calibration_coefficients((char *)calibration_data_buffer, sizeof(calibration_data_buffer));
+    // bmp180.init_bmp180_calibration_coefficients((char *)calibration_data_buffer, sizeof(calibration_data_buffer));
+    bmp180.init_bmp180_calibration_coefficients((char *)adaFruit9DoFImu.get_calibration_buffer_address(),
+                                                adaFruit9DoFImu.get_calibration_buffer_size());
 
     float calculated_temperature = 0.0f;
     bmp180.calculate_temperature(long_uncompensated_temperature, calculated_temperature);
