959fc7c8175e984f5e2eb234010ae34fa55fc616
Daniel Rowan <omnisurfer@gmail.com>
2021-05-09 20:22:52 -0700

    added a conditional variable and mutex. significantly restructured code for clarity. start stubs for other sensors

--- sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9d0f_imu.h
+++ sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9d0f_imu.h
@@ -16,80 +16,112 @@
 class AdaFruit9DoFImu {
 
 private:
-    int _i2c_bus_number;
-    int _i2c_device_address;
-    std::string _device_name;
-
-    context_t _ada_i2c_context;
-
-    uint8_t _calibration_data_buffer[11 * 2] = {0};
-
-    uint16_t _long_uncompensated_temperature;
-
-    uint16_t _long_uncompensated_pressure;
-    uint8_t  _short_uncompensated_pressure_xlsb;
-
-    typedef void (*client_callback_function)(int);
-    client_callback_function _client_callback_function;
-
-    bool run_data_capture_thread = false;
-    //std::condition_variable data_capture_thread_cv;
-    //std::mutex data_capture_thread_access_mutex;
-    std::thread data_capture_thread;
-
-    void data_capture_worker();
-
     int close_device() {
 
-        i2c_dev_close(&_ada_i2c_context, _i2c_bus_number);
+        i2c_dev_close(&_bmp180_i2c_context, _bmp180_i2c_bus_number);
 
         return 0;
     }
 
 public:
-    AdaFruit9DoFImu(int bus_number, int device_address, std::string device_name, client_callback_function function_pointer) {
-        _i2c_bus_number = bus_number;
-        _i2c_device_address = device_address;
-        _device_name = std::move(device_name);
-
-        _client_callback_function = function_pointer;
-
-        data_capture_thread = std::thread(&AdaFruit9DoFImu::data_capture_worker, this);
-    }
-
-    /*
-     * https://stackoverflow.com/questions/64650981/call-to-implicitly-deleted-copy-constructor-of-class-error
-     */
-    AdaFruit9DoFImu(const AdaFruit9DoFImu&) = delete;
-    AdaFruit9DoFImu& operator=(const AdaFruit9DoFImu&) = delete;
-
-    AdaFruit9DoFImu(AdaFruit9DoFImu&&) noexcept = default;
-    AdaFruit9DoFImu& operator=(AdaFruit9DoFImu&&) noexcept = default;
+    AdaFruit9DoFImu() = default;
 
     ~AdaFruit9DoFImu() {
         std::cout << "destructor called" << std::endl;
 
         this->close_device();
 
-        this->run_data_capture_thread = false;
+        this->run_bmp180_data_capture_thread = false;
 
-        if(data_capture_thread.joinable()) {
-            data_capture_thread.join();
+        if(bmp180_data_capture_thread.joinable()) {
+            bmp180_data_capture_thread.join();
         }
     }
 
     int connect_to_device() {
 
+        int status = 1;
+
+        status &= this->_connect_to_bmp180();
+
+        return status;
+    }
+
+    int init_device() {
+
+        this->_init_bmp180();
+
+        return 1;
+    }
+
+
+    /*
+     * BMP180
+     */
+private:
+    int _bmp180_i2c_bus_number{};
+    int _bmp180_i2c_device_address{};
+    std::string _bmp180_device_name;
+
+    context_t _bmp180_i2c_context{};
+
+    Bmp180 bmp180;
+    uint8_t _bmp180_calibration_data_buffer[11 * 2] = {0};
+    uint16_t _bmp180_long_uncompensated_temperature{};
+    uint16_t _bmp180_long_uncompensated_pressure{};
+    uint8_t  _bmp180_short_uncompensated_pressure_xlsb{};
+
+    typedef void (*bmp180_callback_function)(int);
+    bmp180_callback_function _bmp180_callback_function{};
+
+    bool run_bmp180_data_capture_thread = false;
+    std::condition_variable bmp180_data_capture_thread_run_cv;
+    std::mutex bmp180_data_capture_thread_run_mutex;
+    std::thread bmp180_data_capture_thread;
+
+    void _bmp180_data_capture_worker();
+
+    int _init_bmp180() {
+
+        buffer_t inbound_message = {
+                .bytes = _bmp180_calibration_data_buffer,
+                .size = sizeof(_bmp180_calibration_data_buffer)
+        };
+
+        uint8_t register_address = (Bmp180::Addresses::CalibrationCoefficients::AC1 >> 8) & 0xff;
+        if (i2c_recv(&_bmp180_i2c_context, &inbound_message, register_address)) {
+
+            std::cout << "calibration data: " << std::endl;
+
+            for(int i = 0; i < sizeof(_bmp180_calibration_data_buffer); ++i) {
+                std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)_bmp180_calibration_data_buffer[i] << " ";
+            }
+
+            std::cout << std::endl;
+        }
+
+        this->bmp180.init_bmp180_calibration_coefficients((char *) this->_get_bmp180_calibration_buffer_address(),
+                                                          this->_get_bmp180_calibration_buffer_size());
+
+        std::lock_guard<std::mutex> lk(this->bmp180_data_capture_thread_run_mutex);
+        this->bmp180_data_capture_thread_run_cv.notify_one();
+        this->run_bmp180_data_capture_thread = true;
+
+        return 1;
+    }
+
+    int _connect_to_bmp180() {
+
         /*
          * setup the i2c context and connect
          */
-        _ada_i2c_context = {0};
-        if(!i2c_dev_open(&_ada_i2c_context, _i2c_bus_number, _i2c_device_address)) {
+        _bmp180_i2c_context = {0};
+        if(!i2c_dev_open(&_bmp180_i2c_context, _bmp180_i2c_bus_number, _bmp180_i2c_device_address)) {
             std::cout << "failed to open device\n";
             return 0;
         }
 
-        if(!i2c_is_connected(&_ada_i2c_context)) {
+        if(!i2c_is_connected(&_bmp180_i2c_context)) {
             std::cout << "failed to connect to device\n";
             return 0;
         }
@@ -97,7 +129,46 @@ public:
         return 1;
     }
 
-    int load_mock_calibration_data() {
+    void _get_bmp180_temperature();
+
+    void _get_bmp180_pressure();
+
+    uint16_t _get_bmp180_uncompensated_temperature_count() const {
+        return _bmp180_long_uncompensated_temperature;
+    }
+
+    uint16_t _get_bmp180_uncompensated_pressure() const {
+        return _bmp180_long_uncompensated_pressure;
+    }
+
+    uint8_t _get_bmp180_uncompensated_pressure_xlsb() const {
+        return _bmp180_short_uncompensated_pressure_xlsb;
+    }
+
+    uint8_t* _get_bmp180_calibration_buffer_address() {
+        return _bmp180_calibration_data_buffer;
+    }
+
+    int _get_bmp180_calibration_buffer_size() {
+        return sizeof(_bmp180_calibration_data_buffer);
+    }
+
+public:
+    int config_bmp180(int bus_number, int device_address, std::string device_name, bmp180_callback_function function_pointer) {
+        _bmp180_i2c_bus_number = bus_number;
+        _bmp180_i2c_device_address = device_address;
+        _bmp180_device_name = std::move(device_name);
+
+        _bmp180_callback_function = function_pointer;
+
+        bmp180_data_capture_thread = std::thread(&AdaFruit9DoFImu::_bmp180_data_capture_worker, this);
+
+        bmp180 = Bmp180();
+
+        return 0;
+    }
+
+    int load_mock_bmp180_calibration_data() {
 
         /*
          * RPI4 is little endian, x86 = little endian  (use lscpu | grep -i byte)
@@ -166,7 +237,7 @@ public:
 
         int8_t register_address = 0x00;
 
-        if (i2c_send(&_ada_i2c_context, &outbound_message, register_address)) {
+        if (i2c_send(&_bmp180_i2c_context, &outbound_message, register_address)) {
             std::cout << "sent mock calibration data to device OK\n";
             return 0;
         }
@@ -174,53 +245,56 @@ public:
         return -1;
     }
 
-    int init_device() {
 
-        buffer_t inbound_message = {
-                .bytes = _calibration_data_buffer,
-                .size = sizeof(_calibration_data_buffer)
-        };
+    /*
+     * L3GD20
+     */
+private:
+    int _l3gd20_i2c_bus_number{};
+    int _l3gd20_i2c_device_address{};
+    std::string _l3gd20_device_name;
 
-        uint8_t register_address = (Bmp180::Addresses::CalibrationCoefficients::AC1 >> 8) & 0xff;
-        if (i2c_recv(&_ada_i2c_context, &inbound_message, register_address)) {
+    context_t _l3gd20_i2c_context{};
 
-            std::cout << "calibration data: " << std::endl;
+    int _init_l3gd20() {
 
-            for(int i = 0; i < sizeof(_calibration_data_buffer); ++i) {
-                std::cout << std::hex << std::setfill('0') << std::setw(2) << (int)_calibration_data_buffer[i] << " ";
-            }
-
-            std::cout << std::endl;
-        }
-
-        //start the thread
-        this->run_data_capture_thread = true;
-
-        return 1;
+        return 0;
     }
 
-    void read_temperature();
-
-    void read_pressure();
-
-    uint16_t get_uncompensated_temperature_count() const {
-        return _long_uncompensated_temperature;
+    int _connect_to_l3gd20() {
+        return 0;
     }
 
-    uint16_t get_uncompensated_pressure() const {
-        return _long_uncompensated_pressure;
+public:
+
+    int load_mock_l3gd20_data() {
+        return 0;
     }
 
-    uint8_t get_uncompensated_pressure_xlsb() const {
-        return _short_uncompensated_pressure_xlsb;
+
+    /*
+     * LSM303DLHC
+     */
+private:
+    int _lsm303dlhc_i2c_bus_number{};
+    int _lsm303dlhc_i2c_device_address{};
+    std::string _lsm303dlhc_device_name;
+
+    context_t _lsm303dlhc_i2c_context{};
+
+    int _init_lsm303dlhc() {
+
+        return 0;
     }
 
-    uint8_t* get_calibration_buffer_address() {
-        return _calibration_data_buffer;
+    int _connect_to_lsm303dlhc() {
+        return 0;
     }
 
-    int get_calibration_buffer_size() {
-        return sizeof(_calibration_data_buffer);
+public:
+
+    int load_mock_lsm303dlhc_data() {
+        return 0;
     }
 };
 
--- sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9dof_imu.cpp
+++ sctebot_sensors/adafruit_9d0f_imu/src/adafruit_9dof_imu.cpp
@@ -15,7 +15,7 @@
 #include "adafruit_9d0f_imu.h"
 
 
-void AdaFruit9DoFImu::read_temperature() {
+void AdaFruit9DoFImu::_get_bmp180_temperature() {
 // command temperature read
     // notify bmp180 to read temp reg
     // wait 4.5ms
@@ -30,7 +30,7 @@ void AdaFruit9DoFImu::read_temperature() {
     buffer_t inbound_message;
 
     register_address = Bmp180::Addresses::DataRegisters::CONTROL_MEASUREMENT;
-    if (i2c_send(&_ada_i2c_context, &outbound_message, register_address)) {
+    if (i2c_send(&_bmp180_i2c_context, &outbound_message, register_address)) {
         std::cout << "sent buffer to device OK\n";
         std::this_thread::sleep_for(std::chrono::microseconds (4500));
     }
@@ -43,12 +43,12 @@ void AdaFruit9DoFImu::read_temperature() {
     };
 
     register_address = (Bmp180::Addresses::DataRegisters::OUTPUT >> 8) & 0xff;
-    i2c_recv(&_ada_i2c_context, &inbound_message, register_address);
+    i2c_recv(&_bmp180_i2c_context, &inbound_message, register_address);
 
-    _long_uncompensated_temperature = (uncompensated_temperature[1] << 8) | uncompensated_temperature[0];
+    _bmp180_long_uncompensated_temperature = (uncompensated_temperature[1] << 8) | uncompensated_temperature[0];
 }
 
-void AdaFruit9DoFImu::read_pressure() {
+void AdaFruit9DoFImu::_get_bmp180_pressure() {
     uint8_t control_register_and_start_meas[1] = {0x2E};
     buffer_t outbound_message = {
             .bytes = control_register_and_start_meas,
@@ -70,7 +70,7 @@ void AdaFruit9DoFImu::read_pressure() {
     };
 
     register_address = Bmp180::Addresses::DataRegisters::CONTROL_MEASUREMENT;
-    if (i2c_send(&_ada_i2c_context, &outbound_message, register_address)) {
+    if (i2c_send(&_bmp180_i2c_context, &outbound_message, register_address)) {
         std::cout << "sent buffer to device OK\n";
         std::this_thread::sleep_for(std::chrono::microseconds (4500));
     }
@@ -83,62 +83,50 @@ void AdaFruit9DoFImu::read_pressure() {
     };
 
     register_address = (Bmp180::Addresses::DataRegisters::OUTPUT >> 8) & 0xff;
-    i2c_recv(&_ada_i2c_context, &inbound_message, register_address);
+    i2c_recv(&_bmp180_i2c_context, &inbound_message, register_address);
 
-    _long_uncompensated_pressure = (uncompensated_pressure[0] << 8) | uncompensated_pressure[1];
-    _short_uncompensated_pressure_xlsb = uncompensated_pressure[2];
+    _bmp180_long_uncompensated_pressure = (uncompensated_pressure[0] << 8) | uncompensated_pressure[1];
+    _bmp180_short_uncompensated_pressure_xlsb = uncompensated_pressure[2];
 
 
 }
 
-void AdaFruit9DoFImu::data_capture_worker() {
-    std::cout << "data_capture_worker starting" << std::endl;
+void AdaFruit9DoFImu::_bmp180_data_capture_worker() {
+    std::cout << "_bmp180_data_capture_worker starting" << std::endl;
 
-    /*
-    std::unique_lock<std::mutex> lk(this->data_capture_thread_access_mutex);
+    std::unique_lock<std::mutex> lk(this->bmp180_data_capture_thread_run_mutex);
     std::cout << "waiting to run..." << std::endl;
-    this->data_capture_thread_cv.wait(lk, [this]{
-        return run_data_capture_thread;
-    });
-    */
+    this->bmp180_data_capture_thread_run_cv.wait(lk);
+    lk.unlock();
 
-    /**/
-    while(!this->run_data_capture_thread) {
-        std::cout << "waiting to run..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-    /**/
+    lk.lock();
+    while(this->run_bmp180_data_capture_thread) {
+        lk.unlock();
 
-    while(this->run_data_capture_thread) {
+        this->_get_bmp180_temperature();
 
-        this->read_temperature();
+        this->_get_bmp180_pressure();
 
-        this->read_pressure();
-
-        uint16_t long_uncompensated_temperature = this->get_uncompensated_temperature_count();
-        uint16_t long_uncompensated_pressure = this->get_uncompensated_pressure();
-        uint8_t short_uncompensated_pressure_xlsb = this->get_uncompensated_pressure_xlsb();
-
-        /*
-         * Start calculating measurements with the Bmp180 code
-         */
-        Bmp180 bmp180 = Bmp180();
-        bmp180.init_bmp180_calibration_coefficients((char *) this->get_calibration_buffer_address(),
-                                                    this->get_calibration_buffer_size());
+        uint16_t long_uncompensated_temperature = this->_get_bmp180_uncompensated_temperature_count();
+        uint16_t long_uncompensated_pressure = this->_get_bmp180_uncompensated_pressure();
+        uint8_t short_uncompensated_pressure_xlsb = this->_get_bmp180_uncompensated_pressure_xlsb();
 
         float calculated_temperature = 0.0f;
-        bmp180.calculate_temperature(long_uncompensated_temperature, calculated_temperature);
+        this->bmp180.calculate_temperature(long_uncompensated_temperature, calculated_temperature);
 
         float calculated_pressure = 0.0f;
-        bmp180.calculate_pressure(long_uncompensated_pressure, short_uncompensated_pressure_xlsb, calculated_pressure);
+        this->bmp180.calculate_pressure(long_uncompensated_pressure, short_uncompensated_pressure_xlsb, calculated_pressure);
+
+        this->_bmp180_callback_function(42);
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
+        lk.lock();
     }
 
-    std::cout << "data_capture_worker exiting" << std::endl;
+    std::cout << "_bmp180_data_capture_worker exiting" << std::endl;
 }
 
-void handle_measurements(int some_int) {
+void handle_bmp180_measurements(int some_int) {
     std::cout << "got called to handle data " << some_int << std::endl;
 }
 
@@ -168,28 +156,26 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    AdaFruit9DoFImu adaFruit9DoFImu = AdaFruit9DoFImu(
+    AdaFruit9DoFImu adaFruit9DoFImu;
+
+    adaFruit9DoFImu.config_bmp180(
             i2c_bus_number,
             i2c_device_address,
             "pressure sensor",
-            &handle_measurements
-            );
+            &handle_bmp180_measurements
+    );
 
-    int connect_ok = adaFruit9DoFImu.connect_to_device();
-
-    if(!connect_ok) {
+    if(!adaFruit9DoFImu.connect_to_device()) {
         return 0;
-    }
+    };
 
-    adaFruit9DoFImu.load_mock_calibration_data();
+    adaFruit9DoFImu.load_mock_bmp180_calibration_data();
 
     adaFruit9DoFImu.init_device();
 
     std::cout << "press any key to continue..." << std::endl;
 
     std::cin.get();
-
-    std::cout << "exiting thread" << std::endl;
 
     return 0;
 }
