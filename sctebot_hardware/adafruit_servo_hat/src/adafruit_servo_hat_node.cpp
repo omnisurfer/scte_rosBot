8fdf8968e13d37d9e4984f96c216df2619c0a455
Daniel Rowan <omnisurfer@gmail.com>
2022-02-06 11:39:31 -0800

    forgot to update the ROS node

--- sctebot_hardware/adafruit_servo_hat/src/adafruit_servo_hat_node.cpp
+++ sctebot_hardware/adafruit_servo_hat/src/adafruit_servo_hat_node.cpp
@@ -8,7 +8,22 @@ int main(int argc, char* argv[]) {
 
     std::unique_ptr<Pca9685LEDController> pca9685LedController(new Pca9685LEDController());
 
-    pca9685LedController->init_device();
+    int op_pwm_max_count_cycle = 4095;
+    float op_pwm_on_percent = 0.0;
+    float op_pwm_min_limit_duty_cycle = 0.03;
+    float op_pwm_max_limit_duty_cycle = 0.125;
+    float op_pwm_min_operating_duty_cycle = 0.03;
+    float op_pwm_max_operating_duty_cycle = 0.125;
+    float op_pwm_on_delay = 0.0;
+
+    pca9685LedController->init_device(
+            op_pwm_max_count_cycle,
+            op_pwm_on_delay,
+            op_pwm_min_limit_duty_cycle,
+            op_pwm_max_limit_duty_cycle,
+            op_pwm_min_operating_duty_cycle,
+            op_pwm_max_operating_duty_cycle
+            );
 
     std::cout << "Adafruit Servo Hat node running..." << std::endl;
 
