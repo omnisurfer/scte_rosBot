04d2b511654fbb876b03cf8ce2123b91d4b71125
Daniel Rowan <omnisurfer@gmail.com>
2023-12-02 16:06:04 -0800

    add start of pmw3901 circuit python work

---
+++ sctebot_circuit_python/Adafruit_CircuitPython_pmw3901_SPI/pmw3901_spi.py
@@ -0,0 +1,22 @@
+"""
+`pmw3901_spi`
+================================================================================
+
+SPI driven CircuitPython driver for pmw3901 ported from the pimoroni pmw3901-python driver:
+https://github.com/pimoroni/pmw3901-python
+
+* Author(s): N/A
+
+Implementation Notes
+
+**Hardware:**
+
+* Intended to be used with adafruit FT232H General Purpose USB to GPIO, SPI, I2C bridge device:
+  https://www.adafruit.com/product/2264
+
+**Software and Dependencies:**
+
+* Adafruit Blinka:
+  https://github.com/adafruit/Adafruit_Blinka
+
+"""
