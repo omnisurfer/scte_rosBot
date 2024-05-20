8c0ec3d2c833a647afbe9b9fa10608f54f98d076
Daniel R <daniel@none.com>
2024-05-19 21:06:43 -0700

    Move pmw3901 wrapper code into a class. Created some basic ros params to configure the module. Need to update launch file to use them at some point. Found some references for basic odom sensor, similar to the simulated odom from ackermann node...

--- sctebot_sensors/pmw3901_optical_flow/src/pmw3901_optical_flow_node.py
+++ sctebot_sensors/pmw3901_optical_flow/src/pmw3901_optical_flow_node.py
@@ -5,34 +5,79 @@ import rospy
 from pmw3901 import PMW3901, PAA5100, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
 
 
+'''
+odom node ref:
+- https://github.com/methylDragon/ros-sensor-fusion-tutorial/blob/master/01%20-%20ROS%20and%20Sensor%20Fusion%20Tutorial.md
+- http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
+- https://www.reddit.com/r/robotics/comments/ggn8lr/correct_setup_of_wheel_encoders_and_odometry_in/?rdt=37191
+'''
+
+
+class PMW3901Module:
+
+    class ConfigOptions:
+        sensor_module = {'pmw3901': PMW3901, 'paa5100': PAA5100}
+        sensor_rotation = {'0': 0, '90': 90, '180': 180, '270': 270}
+        sensor_spi_cs_slot = {'front': BG_CS_FRONT_BCM, 'back': BG_CS_BACK_BCM}
+
+    def __init__(self,
+                 module=ConfigOptions.sensor_module['pmw3901'],
+                 spi_port=0,
+                 spi_cs_slot=ConfigOptions.sensor_spi_cs_slot['back'],
+                 rotation=ConfigOptions.sensor_rotation['0']
+                 ):
+
+        self.sensor_class = module
+
+        self.flo_sensor = self.sensor_class(
+            spi_port=spi_port,
+            spi_cs_gpio=spi_cs_slot
+        )
+        self.flo_sensor.set_rotation(rotation)
+
+    def get_motion(self):
+
+        return self.flo_sensor.get_motion()
+
+
 def main():
+    rospy.init_node('pmw3901', anonymous=True)
+    rate = rospy.Rate(1)
 
-    sensor_module = {'pmw3901': PMW3901, 'paa5100': PAA5100}
-    sensor_rotation = {'0': 0, '90': 90, '180': 180, '270': 270}
-    sensor_spi_slot = {'front': BG_CS_FRONT_BCM, 'back': BG_CS_BACK_BCM}
+    # Get ros parameters
+    _module = rospy.get_param("/module", 'pmw3901')
+    _spi_port = rospy.get_param("/spi_port", 0)
+    _spi_cs_slot = rospy.get_param("/spi_cs_slot", 'back')
+    _sensor_rotation = rospy.get_param("/rotation", '0')
 
-    sensor_class = sensor_module['pmw3901']
+    # check parameters
+    module = PMW3901Module.ConfigOptions.sensor_module.get(_module, 'pmw3901')
+    spi_cs_slot = PMW3901Module.ConfigOptions.sensor_spi_cs_slot.get(_spi_cs_slot, 'back')
+    sensor_rotation = PMW3901Module.ConfigOptions.sensor_rotation.get(_sensor_rotation, '0')
 
-    flo = sensor_class(spi_port=0, spi_cs_gpio=sensor_spi_slot['back'])
-    flo.set_rotation(sensor_rotation['0'])
+    rospy.loginfo(f"Sensor module {module}, spi port/cs-slot {_spi_port}/{spi_cs_slot}, rotation {sensor_rotation}")
+
+    pmw3901_module = PMW3901Module(
+        module=module,
+        spi_port=_spi_port,
+        spi_cs_slot=spi_cs_slot,
+        rotation=sensor_rotation
+    )
 
     tx = 0
     ty = 0
 
-    rospy.init_node('pmw3901', anonymous=True)
-    rate = rospy.Rate(10)
-
     while not rospy.is_shutdown():
-        rospy.loginfo('Running node...')
+        rospy.loginfo('F Running node...')
 
         try:
-            x, y = flo.get_motion()
+            dx, dy = pmw3901_module.get_motion()
         except RuntimeError:
             continue
-        tx += x
-        ty += y
-        print("Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(x, y, tx, ty))
-        time.sleep(5)
+        tx += dx
+        ty += dy
+        print("Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(dx, dy, tx, ty))
+        rate.sleep()
 
 
 if __name__ == '__main__':
