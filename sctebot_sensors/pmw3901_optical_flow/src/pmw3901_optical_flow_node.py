0f4ffcf0e355a4472aee188afd869491b1952a96
Daniel R <daniel@none.com>
2024-06-08 16:03:16 -0700

    start of building crude odom and tf publisher based on linked examples but modified to work with the optical flow sensor. The math is probably not correct given where the sensor is placed, especially when it comes to rotations. Currently banking on the ROS TF framework to take into account the offset from the center of the vehicle and just work out...

--- sctebot_sensors/pmw3901_optical_flow/src/pmw3901_optical_flow_node.py
+++ sctebot_sensors/pmw3901_optical_flow/src/pmw3901_optical_flow_node.py
@@ -2,14 +2,21 @@
 import time
 
 import rospy
+import tf
 from pmw3901 import PMW3901, PAA5100, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
 
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist, Quaternion, Pose, TransformStamped
+from tf.broadcaster import TransformBroadcaster
+
+from math import pi, sin, cos, tan
 
 '''
 odom node ref:
 - https://github.com/methylDragon/ros-sensor-fusion-tutorial/blob/master/01%20-%20ROS%20and%20Sensor%20Fusion%20Tutorial.md
 - http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
 - https://www.reddit.com/r/robotics/comments/ggn8lr/correct_setup_of_wheel_encoders_and_odometry_in/?rdt=37191
+- https://gist.github.com/atotto/f2754f75bedb6ea56e3e0264ec405dcf
 '''
 
 
@@ -42,22 +49,22 @@ class PMW3901Module:
         )
         self.flo_sensor.set_rotation(rotation)
 
-        self.delta_x_raw = 0
-        self.delta_y_raw = 0
+        self.delta_x_count = 0
+        self.delta_y_count = 0
 
-    def get_motion_raw_count(self):
+    def get_motion_delta_count(self):
 
         # TODO turn into thread to update async to request for data?
-        self.delta_x_raw, self.delta_y_raw = self.flo_sensor.get_motion()
+        self.delta_x_count, self.delta_y_count = self.flo_sensor.get_motion()
 
-        return self.delta_x_raw, self.delta_y_raw
+        return self.delta_x_count, self.delta_y_count
 
-    def get_motion_converted(self):
+    def get_motion_delta_meters(self):
 
-        dx_conv = self.delta_x_raw * self.sensor_count_to_distance_tf
-        dy_conv = self.delta_y_raw * self.sensor_count_to_distance_tf
+        delta_x_meters = self.delta_x_count * self.sensor_count_to_distance_tf
+        delta_y_meters = self.delta_y_count * self.sensor_count_to_distance_tf
 
-        return dx_conv, dy_conv
+        return delta_x_meters, delta_y_meters
 
 
 def main():
@@ -84,33 +91,102 @@ def main():
         rotation=sensor_rotation
     )
 
-    tx_raw = 0
-    ty_raw = 0
+    cdx = 0
+    cdy = 0
 
-    tx_conv = 0
-    ty_conv = 0
+    dx = 0.0
+    dy = 0.0
+    th = 0.0
 
+    vx = 0.1
+    vy = -0.1
+    vth = 0.1
+
+    time_now = rospy.Time.now()
+    last_time = rospy.Time.now()
+
+    odom_publisher = rospy.Publisher("odom", Odometry, queue_size=50)
+    odom_broadcaster = tf.TransformBroadcaster()
+
+    rospy.loginfo('Running node...')
     while not rospy.is_shutdown():
-        rospy.loginfo('Running node...')
+
+        data_updated = False
 
         try:
-            dx, dy = pmw3901_module.get_motion_raw_count()
-            dx_conv, dy_conv = pmw3901_module.get_motion_converted()
-        except RuntimeError:
-            continue
-        tx_raw += dx
-        ty_raw += dy
+            delta_x_count, delta_y_count = pmw3901_module.get_motion_delta_count()
+            delta_x, delta_y = pmw3901_module.get_motion_delta_meters()
 
-        tx_conv += dx_conv
-        ty_conv += dy_conv
-        '''print("RAW Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(
-            dx, dy, tx_raw, ty_raw
+            if delta_y == 0.0:
+                delta_th = 0.0
+            else:
+                delta_th = tan(delta_x/delta_y)
+
+            time_now = rospy.Time.now()
+
+            cdx += delta_x_count
+            cdy += delta_y_count
+            dx += delta_x
+            dy += delta_y
+            th += delta_th
+
+            # compute odometry given velocities of the sense
+            dt = (time_now - last_time).to_sec()
+
+            vx = dx / dt
+            vy = dy / dt
+            vth = th / dt
+
+            quaternion = Quaternion()
+            quaternion.x = 0.0
+            quaternion.y = 0.0
+            quaternion.z = sin(th * 0.5)
+            quaternion.w = cos(th * 0.5)
+
+            odom_trans = TransformStamped()
+            odom_trans.header.stamp = time_now
+            odom_trans.header.frame_id = 'odom'
+            odom_trans.child_frame_id = 'chassis'
+            odom_trans.transform.translation.x = dx
+            odom_trans.transform.translation.y = dy
+            odom_trans.transform.translation.z = 0.0
+            odom_trans.transform.rotation = quaternion
+
+            odom_broadcaster.sendTransformMessage(odom_trans)
+
+            odom = Odometry()
+            odom.header.frame_id = 'odom'
+            odom.child_frame_id = 'chassis'
+            odom.header.stamp = time_now
+            odom.pose.pose.position.x = dx
+            odom.pose.pose.position.y = dy
+            odom.pose.pose.position.z = 0.0
+            odom.pose.pose.orientation = quaternion
+            odom.twist.twist.linear.x = vx
+            odom.twist.twist.linear.y = 0.0
+            odom.twist.twist.angular.z = vth
+
+            odom_publisher.publish(odom)
+
+            data_updated = True
+            last_time = rospy.Time.now()
+
+        except RuntimeError:
+            print('RUNTIME_ERROR')
+            continue
+
+        if data_updated:
+            print(
+                f"delta_x_count {delta_x_count}, "
+                f"delta_y_count {delta_y_count} - "
+                f"delta_x {delta_x:.3f} "
+                f"delta_y {delta_y:.3f} - "
+                f"delta_th {delta_th}"
             )
-        )'''
-        print("Absolute: x {:03f} y {:03f} | Relative: x {:03f} y {:03f} ".format(
-            tx_conv, ty_conv, dx_conv, dy_conv
-            )
-        )
+
+            print(f"vx {vx:.3f} vy {vy:.3f} - vth {vth:.3f} - dt {dt}")
+            print(f"cdx {cdx} cdy {cdy} - dx {dx:.3f} dy {dy:.3f} - th {th:.3f}")
+
         rate.sleep()
 
 
